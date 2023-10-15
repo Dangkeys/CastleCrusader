6202db84715ea9328c26f9fed7fb9ac89c035070
Dangkeys <TajdangZaa@gmail.com>
2023-10-15 17:02:03 +0700

    Added destroy balista mechanic when player place balista unfairly

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1babcd13535967d1ccd088613c4569534b59752d49388a3fa4ff99ad8c2e76a
-size 1170
+oid sha256:bf2443200d3db475b84691a24745c004b0f0ba0c36618f367e5f4a31d8a5168a
+size 1233
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07643ff9ed653563e8dd819fca841ac99c0785e407a6954e11b46581a8bf52ec
-size 1280
+oid sha256:e220615b8ac42126139717dde32abfffb86506e2521d5de1cb187ecb47b43f9b
+size 1938
